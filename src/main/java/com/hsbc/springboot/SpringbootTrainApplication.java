31331589f750a646d0cf55d223263e6946deb215
Damon <542064784@qq.com>
2018-07-31 17:09:44 +0800

    init project

---
+++ src/main/java/com/hsbc/springboot/SpringbootTrainApplication.java
@@ -0,0 +1,12 @@
+package com.hsbc.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTrainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootTrainApplication.class, args);
+    }
+}
