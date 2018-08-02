691361ae4986e72c401c30c9669b397ba844a100
zhangsun.jiankun <mr_zhangsun@outlook.com>
2018-08-02 17:44:12 +0800

    add datasource config

---
+++ src/main/java/com/hsbc/springboot/controller/FileUploadController.java
@@ -0,0 +1,13 @@
+package com.hsbc.springboot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FileUploadController {
+
+    @GetMapping("/")
+    public String indexUI() {
+        return "Hello World";
+    }
+}
