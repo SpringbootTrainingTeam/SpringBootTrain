4cc172b035492dc37ae9fd819aab98219404b523
我爱吃面条 <33244071+CC19930312@users.noreply.github.com>
2018-09-27 21:13:33 +0800

    this is my first commit

--- src/main/java/com/hsbc/springboot/SpringbootTrainApplication.java
+++ src/main/java/com/hsbc/springboot/SpringbootTrainApplication.java
@@ -3,6 +3,9 @@ package com.hsbc.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * this is Git&idea test
+ */
 
 @SpringBootApplication
 public class SpringbootTrainApplication {
