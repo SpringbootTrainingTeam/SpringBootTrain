f907e09f53d4d27658d77a61fe000faf58bbaea9
我爱吃面条 <33244071+CC19930312@users.noreply.github.com>
2018-09-27 21:20:07 +0800

    delete a all

--- src/main/java/com/hsbc/springboot/SpringbootTrainApplication.java
+++ src/main/java/com/hsbc/springboot/SpringbootTrainApplication.java
@@ -3,10 +3,6 @@ package com.hsbc.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * this is Git&idea test
- * this is a great software
- */
 
 @SpringBootApplication
 public class SpringbootTrainApplication {
