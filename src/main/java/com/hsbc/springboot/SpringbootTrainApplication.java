121efc174c1944a5638ed2ec8369262bfc8aa1e0
我爱吃面条 <33244071+CC19930312@users.noreply.github.com>
2018-09-27 21:02:00 +0800

    this is my git test

--- src/main/java/com/hsbc/springboot/SpringbootTrainApplication.java
+++ src/main/java/com/hsbc/springboot/SpringbootTrainApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringbootTrainApplication {
+    //this is test
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootTrainApplication.class, args);
