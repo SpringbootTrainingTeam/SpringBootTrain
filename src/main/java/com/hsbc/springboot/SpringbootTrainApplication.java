0960036a6bbfba8e691fb17a156c7515c2842b1a
我爱吃面条 <33244071+CC19930312@users.noreply.github.com>
2018-09-27 21:05:26 +0800

    this is my git test

--- src/main/java/com/hsbc/springboot/SpringbootTrainApplication.java
+++ src/main/java/com/hsbc/springboot/SpringbootTrainApplication.java
@@ -3,9 +3,6 @@ package com.hsbc.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * 测试如何使用Git和idea结合
- */
 
 @SpringBootApplication
 public class SpringbootTrainApplication {
